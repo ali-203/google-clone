dc42263d8c8e31351872fdda2fefd476622a9c8a
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-06 02:00:23 +0500

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,10 @@ function Home() {
     return (
         <div>
             <h1>This is HOMEPAGE</h1>
+            <div className="home_header">
+
+            </div>
+            <div className="home_body"></div>
         </div>
     );
 }
