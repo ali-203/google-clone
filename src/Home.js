a108ee204ea4d890365794e15c4a9f07208a6c3e
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-09 01:43:07 +0500

    update

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,15 @@ import { Link } from "react-router-dom";
 function Home() {
     return (
         <div className="home">
-            <h1>This is HOMEPAGE</h1>
             <div className="home_header">
             <div className="home_headerLeft">
-            <Link to="/about">about</Link>
-            <Link to="/about">about</Link>
+            <Link to="/about">About</Link>
+            <Link to="/about">Store</Link>
 
             </div>
             <div className="home_headerRight">
-            <Link to="/about">about</Link>
-            <Link to="/about">about</Link>
+            <Link to="/about">Gmail</Link>
+            <Link to="/about">Image</Link>
             </div>
 
             </div>
