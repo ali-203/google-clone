5fe37f18d2fe29cf4b495cb9a542aaffe9524e86
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-10 23:51:31 +0500

    add history

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from '@material-ui/core';
+import { useHistory } from "react-router-dom";
+
+
 
 function Search() {
+    const [input, setInput] = useState("");
+    const history = useHistory();
+
+    const search = (e) => {
+        e.preventDefault();
+
+        console.log("you hit the search button >>", input)
+        history.push("/search")
+    };
+
     return (
-        <div className='search'>
+        <form className='search'>
         <div className="search_input">
             <SearchIcon className="search_inputIcon" />
-            <input />
+            <input value={input} onChange={e => setInput(e.target.value)}/>
             <MicIcon />
         </div>
             <div className="search_buttons">
-            <Button variant="outlined">Google Search</Button>
+            <Button type="submit" onClick={search} variant="outlined">Google Search</Button>
             <Button variant="outlined">I'm Feeling Lucky</Button>
 
             </div>
-        </div>
-    )
+        </form>
+    );
 }
 
 export default Search;
