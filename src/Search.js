5f2619c446e9a7d64c10b0c217c5f983c9ac129d
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-10 22:21:36 +0500

    update search

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from '@material-ui/core';
 
 function Search() {
     return (
@@ -11,7 +12,11 @@ function Search() {
             <input />
             <MicIcon />
         </div>
-            
+            <div className="search_buttons">
+            <Button variant="outlined">Google Search</Button>
+            <Button variant="outlined">I'm Feeling Lucky</Button>
+
+            </div>
         </div>
     )
 }
