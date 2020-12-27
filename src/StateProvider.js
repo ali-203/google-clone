2f93db46aabe2bf4cf5ed1757a084e6522cd3489
ali-203 <62180010+ali-203@users.noreply.github.com>
2020-12-27 23:19:37 +0500

    updated redux

---
+++ src/StateProvider.js
@@ -0,0 +1,11 @@
+import React, { createContext, useContext, useReducer } from "react";
+//Preparing the Data Layer
+
+export const StateContext = createContext();
+export const StateProvider = ({
+    reducer, initialStatem, children
+}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}></StateContext.Provider>
+);
+//Hooks which allows us to pull information from te data layer
+export const useStateValue = () => useContext(StateContext);
