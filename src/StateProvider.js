52b856e4db7cbe741459ff1a68a96210602833bd
ali-203 <62180010+ali-203@users.noreply.github.com>
2021-01-24 21:11:06 +0500

    updated state

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -3,8 +3,7 @@ import React, { createContext, useContext, useReducer } from "react";
 
 export const StateContext = createContext();
 export const StateProvider = ({
-    reducer, initialStatem, children
-}) => (
+    reducer, initialState, children}) => (
     <StateContext.Provider value=  {useReducer(reducer, initialState)}></StateContext.Provider>
 );
 //Hooks which allows us to pull information from te data layer
